220e92f8938abcddee0ab87d90e326ec551d9a97
Nanthakumar <146550422+Nanthu-98@users.noreply.github.com>
2023-10-07 11:12:25 +0530

    Add files via upload

---
+++ POS.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+   int n,a[50],N,i,position;
+   clrscr();
+   printf("Enter the number of elements in the array: ");
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+       printf("Enter the element arr[%d]: ",i);
+       scanf("%d",&a[i]);
+   }
+   printf("Original array:{ ",n);
+   for(i=0;i<n;i++){
+       printf("%d ",a[i]);
+   }
+   printf("}\nEnter the position for deleting element: ");
+   scanf("%d",&position);
+   for(i=position-1;i<=n;i++){
+       a[i]=a[i+1];
+   }
+   n--;
+   printf("Updated array:{ ",n);
+   for(i=0;i<n;i++){
+       printf("%d ",a[i]);
+   }
+   printf("}");
+   getch();
+}
